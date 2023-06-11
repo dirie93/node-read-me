4a58971c1e31837736812e54d0f03134b3a2e868
dirie93 <mdirie19@gmail.com>
2023-06-11 20:06:20 +0100

    created empty array to store questions, and a function to write the README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,23 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Importing file
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README Generated");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
