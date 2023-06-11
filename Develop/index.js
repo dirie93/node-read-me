b7604e288c64a64f36558a91034e2b10d621de58
dirie93 <mdirie19@gmail.com>
2023-06-11 20:17:32 +0100

    set up index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -20,7 +20,60 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      [
+        {
+          name: "projectTitle",
+          message: "Please enter the title of your project:",
+        },
+        {
+          name: "description",
+          message: "Please provide a description of your project:",
+        },
+        {
+          name: "installation",
+          message:
+            "Please provide the installation instructions for your project:",
+        },
+        {
+          name: "usage",
+          message: "Please describe how to use your project:",
+        },
+        {
+          name: "contribution",
+          message:
+            "Please provide the contribution guidelines for your project:",
+        },
+        {
+          name: "test",
+          message: "Please provide the test instructions for your project:",
+        },
+        {
+          type: "list",
+          name: "license",
+          message: "Please select a license for your project:",
+          choices: ["Apache-2.0", "GPL-3.0", "MIT", "Unlicense"],
+        },
+        {
+          name: "github",
+          message: "Please enter your GitHub username:",
+        },
+        {
+          name: "email",
+          message: "Please enter your email address:",
+        },
+      ],
+    ])
+    .then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      writeToFile("./output/README.md", readmeContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 // Function call to initialize app
 init();
